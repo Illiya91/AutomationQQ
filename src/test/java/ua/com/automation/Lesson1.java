5991f9df9897461427634e0a9fdaaa6dea31306c
Illiya91 <fortune.illiya@gmail.com>
2022-09-22 18:34:46 +0300

    My home work 1

---
+++ src/test/java/ua/com/automation/Lesson1.java
@@ -0,0 +1,29 @@
+package ua.com.automation;
+
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
+
+    /**
+     * Unit test for simple App.
+     */
+    public class Lesson1
+    {
+        /**
+         * Rigorous Test :-)
+         */
+        @Test
+        public void shouldAnswerWithTrue() throws InterruptedException {
+
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://rozetka.com.ua");
+            Thread.sleep(9999);
+            driver.close();
+        }
+    }
+
